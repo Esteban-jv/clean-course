360356dc95471170a5c163d81ef5fe68845b8d8e
Esteban Juarez <esteban.juarez@owners-link.com>
2025-08-12 10:11:33 -0500

    High and low coupling

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 // import './clean-code/04-homework'
 import './clean-code/05-dry'
 import './clean-code/07-classes-tarea'
+import './code-smells/02-low-coupling'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
