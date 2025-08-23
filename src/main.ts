87a857f66270f995e90598adde1dee26bb111c8c
Esteban-jv <ejv.developer@gmail.com>
2025-08-23 17:38:03 -0500

    OPC AND LSP

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 // import './clean-code/05-dry'
 // import './clean-code/07-classes-tarea'
 // import './code-smells/02-low-coupling'
-import './solid/02-open-close-a'
+import './solid/03-liskov-a'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
